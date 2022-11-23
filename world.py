c025c0a52e64c88c958e0877468e98a176c3227a
Ryael <Rayrah@live.com>
2022-11-23 23:54:20 +0000

    Add core class for Room In addition to the Room class, add instantiation for a room with x and y coordinates.

--- world.py
+++ world.py
@@ -6,3 +6,17 @@ Code is for a terminal of 80 characters wide and 24 rows high.
 This module contains all the world details, lore, and location of items.
 
 """
+
+
+class Room:
+    """
+    The core class for a room within the world.
+    """
+    def __init__(self, xco, yco):
+        """Creates an instance of a new room.
+        Parameter xco: the x-coordinate of the room.
+        Parameter yco: the y-coordinate of the room.
+        """
+        self.xco = xco
+        self.yco = yco
+
