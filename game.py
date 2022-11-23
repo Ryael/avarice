af197f6b862468776c13946fd3c39baf04198909
Ryael <Rayrah@live.com>
2022-11-23 21:03:13 +0000

    Add docstring for all modules

--- game.py
+++ game.py
@@ -1,3 +1,12 @@
+"""
+Project Portfolio 3 (Python): "Avarice"
+
+Developed and written by Andrey Belyakov, November 2022.
+Code is for a terminal of 80 characters wide and 24 rows high.
+This module contains the game-loop and global variables.
+
+"""
+
 
 def p_t(text):
     """
---
+++ monster.py
@@ -0,0 +1,7 @@
+"""
+Project Portfolio 3 (Python): "Avarice"
+
+Developed and written by Andrey Belyakov, November 2022.
+Code is for a terminal of 80 characters wide and 24 rows high.
+This module contains the monster logic.
+"""
