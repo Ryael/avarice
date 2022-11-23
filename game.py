6a2366232659a279641e362cab9bcf82afeaebb3
Ryael <Rayrah@live.com>
2022-11-23 22:28:51 +0000

    Expand start_game() function Add a choice for the user to start the game, request a name, and add another choice for the user to decide if they want to view the intro scene.

--- game.py
+++ game.py
@@ -42,6 +42,11 @@ def start_game():
     """
     Starts the game, called at the end of game.py.
     Prints the game title.
+    Lets the user decide whether to play or not.
+    If they decline, this is noted and the game ends.
+    If they accept, they are asked to provide a name.
+    The user is given a choice whether to view the intro,
+    after which the game begins.
     """
     p_t("""\033[38;2;150;95;143m
     █████╗ ██╗   ██╗ █████╗ ██████╗ ██╗ ██████╗███████╗
@@ -50,6 +55,39 @@ def start_game():
     ██╔══██║╚██╗ ██╔╝██╔══██║██╔══██╗██║██║     ██╔══╝
     ██║  ██║ ╚████╔╝ ██║  ██║██║  ██║██║╚██████╗███████╗
     ╚═╝  ╚═╝  ╚═══╝  ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝ ╚═════╝╚══════╝ \033[0m\n""")
+    # Lets the user decide if they want to play the game and requests a name.
+    start_choice = get_str("Investigator, do you accept the task? (Y/N):")
+    if start_choice.lower() == "y" or start_choice.lower() == "yes":
+        p_t("This task isn't for the faint of hearted,")
+        p_t("but we appreciate your help in this matter.")
+        p_t("We're sure you're the right person for this job.")
+        name = get_str("Could you please state your name for the contract?")
+        p_t(f"Very well, Inspector {name}. Let's begin.")
+        view_intro = False
+        while not view_intro:
+            intro_choice = get_str("Would you like to see the intro? (Y/N):")
+            if intro_choice.lower() == "yes" or intro_choice.lower() == "y":
+                p_t("Play intro scene")
+                p_t("This will describe the back story of the Investigator")
+                p_t("Their motives, why they approached, who approached them")
+                p_t("and ultimately what their goal is")
+                p_t("This will lead directly to the start of the game.")
+            elif intro_choice.lower() == "no" or intro_choice.lower() == "n":
+                first_scene()
+            else:
+                print("Please answer the question, yes or no will suffice.")
+    elif start_choice.lower() == "n" or start_choice.lower() == "no":
+        p_t("""\033[38;2;150;95;143m
+    ████████╗██╗  ██╗███████╗    ███████╗███╗   ██╗██████╗
+    ╚══██╔══╝██║  ██║██╔════╝    ██╔════╝████╗  ██║██╔══██╗
+       ██║   ███████║█████╗      █████╗  ██╔██╗ ██║██║  ██║
+       ██║   ██╔══██║██╔══╝      ██╔══╝  ██║╚██╗██║██║  ██║
+       ██║   ██║  ██║███████╗    ███████╗██║ ╚████║██████╔
+            \033[0m\n""")
+        p_t("Perhaps another person would be better suited.")
+        p_t("Farewell, Investigator.")
+    else:
+        print("Please answer the question, yes or no will suffice.")
 
 
 start_game()
