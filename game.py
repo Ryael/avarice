46c17e8e1db5e565f134154473b4ede9cbbbf3f6
Ryael <Rayrah@live.com>
2022-11-23 20:52:52 +0000

    Add p_t function Allow longer text strings throughout the code.

--- game.py
+++ game.py
@@ -1,9 +1,18 @@
+
+def p_t(text):
+    """
+    Print a line of text.
+    The function name is abbreviated to permit for longer text strings.
+    For clarity: "p_t" stands for print text.
+    """
+    print(text)
+
+
 def start_game():
     """
     Starts the game, called at the end of game.py.
     Prints the game logo.
     """
-
     print("""\033[38;2;150;95;143m
     █████╗ ██╗   ██╗ █████╗ ██████╗ ██╗ ██████╗███████╗
     ██╔══██╗██║   ██║██╔══██╗██╔══██╗██║██╔════╝██╔════╝
