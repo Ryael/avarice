070074e0b74475cdee0157719fc71281d5a3b044
Ryael <Rayrah@live.com>
2022-11-24 02:14:03 +0000

    Remove blankspace in docustrings

--- player.py
+++ player.py
@@ -4,5 +4,4 @@ Project Portfolio 3 (Python): "Avarice"
 Developed and written by Andrey Belyakov, November 2022.
 Code is for a terminal of 80 characters wide and 24 rows high.
 This module contains the logic dictating everything concerning the player.
-
 """
