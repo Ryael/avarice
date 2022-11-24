5c6f80ba3e10667de840422e3805c89e757cb3a3
Ryael <Rayrah@live.com>
2022-11-24 02:44:26 +0000

    Add move() methods The move() method tracks changes in the x,y coordinates. The specific move() that address movement in the cardinal directions, represent changes in the x and y-directions.

--- player.py
+++ player.py
@@ -18,3 +18,40 @@ class Player:
         """
         self.x_coord = 1
         self.y_coord = 2
+
+    def move(self, dx_coords, dy_coords):
+        """
+        The move method is called using named parameteres, these parameteres
+        are from math and represent changes in x and y values. This method
+        accepts a change in the x and/or y directions.
+        """
+        self.x_coord += dx_coords
+        self.y_coord += dy_coords
+
+    def move_north(self):
+        """
+        This method shows the amount of the change in the
+        y-direction (negative).
+        """
+        self.move(dx_coords=0, dy_coords=-1)
+
+    def move_south(self):
+        """
+        This method shows the amount of the change in the
+        y-direction (positive).
+        """
+        self.move(dx_coords=0, dy_coords=1)
+
+    def move_west(self):
+        """
+        This method shows the amount of the change in the
+        x-direction (negative).
+        """
+        self.move(dx_coords=-1, dy_coords=0)
+
+    def move_east(self):
+        """
+        This method shows the amount of the change in the
+        x-direction (positive).
+        """
+        self.move(dx_coords=1, dy_coords=0)
