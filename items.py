4718476abff2ab40f6cf1e23f9f1951941fbc2e8
Ryael <Rayrah@live.com>
2022-11-27 17:21:26 +0000

    Add `Item` class

--- items.py
+++ items.py
@@ -5,3 +5,13 @@ Developed and written by Andrey Belyakov, November 2022.
 Code is for a terminal of 80 characters wide and 24 rows high.
 This module contains the items present in the game as well as the inventory.
 """
+
+
+class Item:
+    """
+    The base class for all items within the game.
+    """
+    def __init__(self, name, desc):
+        "Creates an instance of the item with a name and a description."
+        self.name = name
+        self.desc = desc
