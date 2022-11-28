8905ca4358eb9c13e2766a45fc723c3f9092a8cc
Ryael <Rayrah@live.com>
2022-11-28 02:10:35 +0000

    Update `Path` to use `Position`
    Add method to check if this path exists between two rooms.

--- path.py
+++ path.py
@@ -3,7 +3,7 @@ Project Portfolio 3 (Python): "Avarice"
 
 Developed and written by Andrey Belyakov, November 2022.
 Code is for a terminal of 80 characters wide and 24 rows high.
-This module contains the room generation logic.
+This module contains the Path class.
 """
 
 
@@ -11,14 +11,25 @@ class Path:
     """
     The core path for the map within the world.
     """
-    def __init__(self, desc, r1_coords, r2_coords, lock=False, block=False):
+    def __init__(self, room1, room2, desc, locked=False, blocked=False):
         """
          Creates an instance of a new path.
          Parameter desc = description of the path.
-         Parameters r1_coords & r2_coords = path between two rooms.
+         Parameters room1 & room2 = path between two rooms.
          Parameters lock and block: locked and blocked paths.
         """
         self.desc = desc
-        self.between = [r1_coords, r2_coords]
-        self.lock = lock
-        self.block = block
+        self.room1 = room1
+        self.room2 = room2
+        self.locked = locked
+        self.blocked = blocked
+
+    def exists(self, pos, dest):
+        """
+        Checks if room coordinated exist in this path
+        """
+        if (pos in [self.room1, self.room2] and
+           dest in [self.room1, self.room2]):
+            return True
+
+        return False
