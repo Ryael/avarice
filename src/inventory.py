032791c55735fdd7b6a1ea5f1c71d9d4d673060d
Ryael <Rayrah@live.com>
2022-12-03 18:09:11 +0000

    Add Inventory dataclass
    Add methods for manipulating Inventory

---
+++ src/inventory.py
@@ -0,0 +1,38 @@
+"""
+Project Portfolio 3 (Python): "Avarice"
+
+Developed and written by Andrey Belyakov, November 2022.
+Code is for a terminal of 80 characters wide and 24 rows high.
+This module contains the logic dictating everything concerning the Invetories.
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Inventory:
+    """
+    Contains items in a dictionary
+    """
+    items: dict
+
+    def add_item(self, identifier, item):
+        """
+        Adds an item to the dictionary
+        """
+        self.items[identifier] = item
+
+    def remove_item(self, identifier):
+        """
+        Removes and returns an Item from Inventory
+        """
+        item = self.items[identifier]
+        self.items[identifier] = None
+
+        return item
+
+    def get_item(self, identifier):
+        """
+        returns an Item from Inventory
+        """
+        return self.items[identifier]
