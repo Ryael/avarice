d6c2966c337765719bbcdc9998597851c85bdfef
Ryael <Rayrah@live.com>
2022-12-04 17:56:06 +0000

    Add `room_desc` to Item

--- src/item.py
+++ src/item.py
@@ -6,7 +6,7 @@ Code is for a terminal of 80 characters wide and 24 rows high.
 This module contains the items present in the game as well as the inventory.
 """
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from helper import build_str
 
 
@@ -16,10 +16,17 @@ class Item:
     The base class for all items within the game.
     """
     name: str
-    desc: list
+    desc: list = field(default_factory=list)
+    room_desc: list = field(default_factory=list)
 
     def description(self):
         """
         Returns description as a single string.
         """
         return build_str(self.desc)
+
+    def room_description(self):
+        """
+        Returns description as a single string.
+        """
+        return build_str(self.room_desc)
